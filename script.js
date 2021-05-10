38054d9850ee596267ea463fdd94be57d9d024a2
Bernadette Figueroa <42790618+berniefigs@users.noreply.github.com>
2021-05-10 19:50:32 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggle_visibility(id1, id2) {  
+   var e = document.getElementById(id1);
+   var e2 = document.getElementById(id2);
+   if(e.style.display == 'none') {                
+      e.style.display = 'block';
+      e.style.display = 'block'; //block will show the conain in div            
+      e2.style.display = 'none';
+   }
+   else {
+      e.style.display = 'none';            
+      e2.style.display = 'block';
+   }              
+}
